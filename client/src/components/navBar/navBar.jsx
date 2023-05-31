62c5d88b65e81c2919e4a6f90e15156bd3bcfabb
Juan Pablo Delgado Nivia <juanPDN@github.com>
2023-05-31 18:34:42 -0500

    PI Food

--- client/src/components/navBar/navBar.jsx
+++ client/src/components/navBar/navBar.jsx
@@ -4,7 +4,17 @@ import SearchBar from "../searchBar/searchBar";
 function NavBar() {
     return (
         <nav>
-            <SearchBar/>
+            <select name="diets" id="diets">
+                <option value="null" selected disabled>--Diets--</option>
+                <option>vegan</option>
+                <option>ovo</option>
+            </select>
+            <select name="origin" id="origin">
+                <option value="null" selected disabled>--Origin--</option>
+                <option value="db">My Recipes</option>
+                <option value="api" >Recipes</option>
+            </select>
+            <SearchBar />
         </nav>
     );
 }
