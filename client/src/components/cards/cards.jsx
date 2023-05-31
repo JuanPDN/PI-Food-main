b4af987f838f84019a6b467f7c05d4525811cff4
Juan Pablo Delgado Nivia <juanPDN@github.com>
2023-05-30 19:54:35 -0500

    PI Food

--- client/src/components/cards/cards.jsx
+++ client/src/components/cards/cards.jsx
@@ -2,18 +2,22 @@ import Card from "../card/card";
 
 function Cards(props) {
 
-    console.log(props);
     const recipes = props.recipe
-    console.log(recipes);
-    return ( 
+    return (
         <div>
             {
-                recipes.map((recipe)=>
-                <Card/>
+                recipes.map((recipe) =>
+                    <Card
+                        id={recipe.id}
+                        name={recipe.name}
+                        image={recipe.image}
+                        summary={recipe.summary}
+                        healthScore={recipe.healthScore}
+                    />
                 )
             }
         </div>
-     );
+    );
 }
 
 export default Cards;
