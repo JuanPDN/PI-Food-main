7e3ec0669be14a010cb6bf9b3107eb5573ce3d30
Juan Pablo Delgado Nivia <juanPDN@github.com>
2023-06-02 16:11:57 -0500

    PI Food

--- client/src/components/form/form.jsx
+++ client/src/components/form/form.jsx
@@ -1,30 +1,64 @@
+import { useState } from "react";
 
 
 
 function Form() {
+
+    const [recipe, setRecipe] = useState({
+        name: '',
+        image: '',
+        summary: '',
+        healtScore: '',
+        stepToStep: '',
+        diets: ''
+    })
+
+    const [error, setError] = useState({
+        name: '',
+        summary: '',
+        healtScore: '',
+        stepToStep: '',
+        diets: ''
+    })
+
+    const handleChange = (event) => {
+        setRecipe({
+            ...recipe,
+            [event.target.name]:event.target.value
+        })
+        setError({
+            ...recipe,
+            [event.target.name]:event.target.value
+        })
+    }
+
+    
+
+
     return (
         <div>
             <form>
                 <legend>Your recipe</legend>
 
                 <label for='name'>Name</label>
-                <input id='name' type="text" />
+                <input name='name' type="text" onChange={handleChange} value={recipe.name}/>
 
                 <label for='imageUpload'>Image</label>
-                <input type="file" id="imageUpload" accept="image/*"/>
+                <input type="file" name="imageUpload" accept="image/*"/>
 
                 <label for='summary'>Summary</label>
-                <textarea name="summary" id="summary" cols="30" rows="10"></textarea>
+                <textarea name="summary" id="summary" onChange={handleChange} cols="30" rows="10" value={recipe.summary}></textarea>
 
-                <label for='score'>healt Score</label>
-                <input id="score" type="number" min='0' />
+                <label for='healtScore'>healt Score</label>
+                <input name="healtScore" onChange={handleChange} type="number" min='0' value={recipe.healtScore}/>
 
                 <label for="stepToStep">Step to Step</label>
-                <textarea name="stepToStep" id="stepToStep" cols="30" rows="10"></textarea>
-                
-                <select name="diet" id="diet">
-                    <option value="diets">diets</option>
-                </select>
+                <textarea name="stepToStep" onChange={handleChange} id="stepToStep" cols="30" rows="10" value={recipe.stepToStep}></textarea>
+
+                <label for="checkbox">Diets</label>
+                <input type="checkbox" id="checkbox" name="diets" value="1"></input>
+
+                <button>Crear</button>
             </form>
         </div>
     );
