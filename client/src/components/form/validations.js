b254dbb310bcb92ce48c3c19b2882aec07a524cc
Juan Pablo Delgado Nivia <juanPDN@github.com>
2023-06-02 19:12:40 -0500

    PI Food

---
+++ client/src/components/form/validations.js
@@ -0,0 +1,20 @@
+const errors = {
+    empy: 'el campo no puede estar vacio, campo obligatorio',
+    choseOne: 'debe seleccionar al menos una dieta',
+    score: 'el healt Score debe ser mayor a 0'
+}
+
+
+export const validations = ({ name, summary, healtScore, stepToStep, diets }) => {
+    const error = {}
+    if (name.length === 0 || summary.length === 0 || healtScore.length === 0 || stepToStep.length === 0 || diets.length === 0) {
+        error.long = errors.empy
+    }
+    if (diets.length <= 0) {
+        error.chose = errors.choseOne
+    }
+    if (healtScore <= 0) {
+        error.score = errors.score
+    }
+    return error
+}
